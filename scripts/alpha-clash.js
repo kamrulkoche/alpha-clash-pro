5c9546803d27afa25d5bf618b05031c42b8ba71f
kamrulkoche <kamrulkoche@gmail.com>
2024-03-23 14:19:48 +0600

    onclick even

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,20 @@
-function play() {
-    console.log("hello");
+// function play() {
+//     // step-1: hide the home screen. to hide the screen and the class hidden to the home section
+//     const homeSection = document.getElementById("home-screen");
+//     //console.log(homeSection);
+//     homeSection.classList.add('hidden');
+//     //console.log(homeSection.classList);
+
+
+//     //show the playground
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+//     //playgroundSection.style.backgroundColor='red'
+//     //console.log(playgroundSection.classList);
+// }
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
